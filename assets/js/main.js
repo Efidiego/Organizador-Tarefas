94cd25db6a8a1063f7ba42b8500cccbec0235b86
Fernando Diego <123587574+Efidiego@users.noreply.github.com>
2024-06-08 12:28:57 -0300

    atualização 1.0.1

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,75 +2,72 @@ const inputTarefa = document.querySelector('.input-tarefa');
 const btnTarefa = document.querySelector('.btn-tarefa');
 const tarefas = document.querySelector('.tarefas');
 
-function criaLi() {
-    const li = document.createElement('li');
-    return li;
+function criaCard() {
+    const div = document.createElement('div');
+    div.classList.add('card-tarefa');
+    return div;
 }
 
-inputTarefa.addEventListener('keypress', function(e){
+inputTarefa.addEventListener('keypress', function(e) {
     if (e.keyCode === 13) {
         if (!inputTarefa.value) return;
         criaTarefa(inputTarefa.value);
     }
 });
 
-
-function criaBotaoApagar(li) {
-    li.innerText += ' ';
-    const botaoApagar = document.createElement('button');
-    botaoApagar.innerText = 'Apagar';
-    botaoApagar.setAttribute('class', 'apagar');
-    botaoApagar.setAttribute('title', 'Apagar esta tarefa');
-
-    li.appendChild(botaoApagar);
- 
-}
-
-function criaTarefa(textoInput) {
-    const li = criaLi();
-    li.innerText = textoInput;
-    tarefas.appendChild(li);
-    inputTarefa.value = '';  // Limpa o campo de entrada após adicionar a tarefa
-    inputTarefa.focus();  // Coloca o foco de volta no campo de entrada
-    criaBotaoApagar(li);
-    salvarTarefas();
-}
-
 btnTarefa.addEventListener('click', function() {
     if (!inputTarefa.value) return;
     criaTarefa(inputTarefa.value);
 });
 
+function criaBotaoApagar(card) {
+    const botaoApagar = document.createElement('button');
+    botaoApagar.innerText = 'Apagar';
+    botaoApagar.setAttribute('class', 'apagar');
+    botaoApagar.setAttribute('title', 'Apagar esta tarefa');
+    card.appendChild(botaoApagar);
+}
+
+function criaTarefa(textoInput) {
+    const card = criaCard();
+    const tarefaTexto = document.createElement('span');
+    tarefaTexto.innerText = textoInput;
+    card.appendChild(tarefaTexto);
+    tarefas.appendChild(card);
+    inputTarefa.value = '';
+    inputTarefa.focus();
+    criaBotaoApagar(card);
+    salvarTarefas();
+}
+
 document.addEventListener('click', function(e) {
     const el = e.target;
-    if(el.classList.contains('apagar')){
-    el.parentElement.remove();
-    salvarTarefas();
+    if (el.classList.contains('apagar')) {
+        el.parentElement.remove();
+        salvarTarefas();
     }
-   
 });
 
 function salvarTarefas() {
-  const liTarefas = document.querySelectorAll('li');
-  const listaDeTarefas = [];
+    const cardsTarefas = document.querySelectorAll('.card-tarefa');
+    const listaDeTarefas = [];
 
-  for (let tarefa of liTarefas) {
-    let tarefaTexto = tarefa.innerText;
-    tarefaTexto = tarefaTexto.replace('Apagar', '').trim();
-    
-    listaDeTarefas.push(tarefaTexto);
+    for (let card of cardsTarefas) {
+        let tarefaTexto = card.querySelector('span').innerText;
+        listaDeTarefas.push(tarefaTexto);
     }
     const tarefasJSON = JSON.stringify(listaDeTarefas);
     localStorage.setItem('tarefas', tarefasJSON);
 }
 
-function adicionaTarefasSalvas () {
+function adicionaTarefasSalvas() {
     const tarefas = localStorage.getItem('tarefas');
-    const listaDeTarefas = JSON.parse(tarefas);
-    
-    for(let tarefa of listaDeTarefas) {
-        criaTarefa(tarefa);
+    if (tarefas) {
+        const listaDeTarefas = JSON.parse(tarefas);
+        for (let tarefa of listaDeTarefas) {
+            criaTarefa(tarefa);
+        }
     }
 }
-adicionaTarefasSalvas();
 
+adicionaTarefasSalvas();
